d57c76bfd1690ac3e4b541d053be43a03a346daf
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-07 15:59:25 -0300

    Login e retorno de tweets

--- src/app.js
+++ src/app.js
@@ -1,13 +1,47 @@
-import express, { json } from 'express';
+import express from 'express';
 import cors from 'cors';
 
 const server = express();
+server.use(express.json());
 server.use(cors());
-server.use(json());
+
+const usuarios = [];
+const tweetes = [];
 
 const PORT = 5000;
 
-server.listen(5000, () => {
+server.post('/sign-up', (req, res) => {
+    const usuario = req.body;
+    usuarios.push(usuario);
+    res.send("OK");
+
+});
+
+server.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    if(usuarios.find(u => u.username === tweet.username)){
+        tweetes.push(tweet);
+        res.send("OK");
+    }else{
+        res.send("UNAUTHORIZED");
+    }
+});
+
+server.get('/tweets', (req, res) => {
+    let ultimosTweets = [];
+    if(tweetes.length <= 10){
+        ultimosTweets = tweetes;
+    }else{
+        ultimosTweets = tweetes.slice(tweetes.length-10, tweetes.length);
+    }
+    const tweetsAvatar = ultimosTweets.map(t => {
+        const usuario = usuarios.find(u => u.username === t.username);
+        return {username: t.username, avatar: usuario.avatar, tweet: t.tweet};
+    });
+    res.send(tweetsAvatar);
+});
+
+server.listen(PORT, () => {
   console.log(`Servidor iniciado na porta: ${PORT}`);
   console.log(`Use: http://localhost:${PORT}`);
 });
